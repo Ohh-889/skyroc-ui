e4458b72582ee7828a766e17d12e4c3953de2edf
Ohh-889 <155351881+Ohh-889@users.noreply.github.com>
2025-12-02 02:04:10 +0800

    fix(menu-variants): 🛠️ update min-width style for menu content to use min-w-max

--- packages/ui/src/components/menu/menu-variants.ts
+++ packages/ui/src/components/menu/menu-variants.ts
@@ -12,7 +12,7 @@ export const menuVariants = tv({
       `data-[disabled]:pointer-events-none data-[disabled]:opacity-50`
     ],
     content: [
-      `z-50 min-w-48 rounded-md mt-1.5 border font-medium bg-popover translate-x-2 text-popover-foreground shadow-md will-change-transform`,
+      `z-50 min-w-max rounded-md mt-1.5 border font-medium bg-popover translate-x-2 text-popover-foreground shadow-md will-change-transform`,
       `data-[state=open]:animate-in data-[state=open]:fade-in-0 data-[state=open]:zoom-in-95`,
       `data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=closed]:zoom-out-95`,
       `data-[side=bottom]:slide-in-from-top-2 data-[side=left]:slide-in-from-right-2 data-[side=right]:slide-in-from-left-2 data-[side=top]:slide-in-from-bottom-2`
